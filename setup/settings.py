725e0fbd7721f201b0d30b2087e80e75d8950e96
arthurgranito <arthurcursotecnico@gmail.com>
2026-08-17 11:22:47 -0300

    commit para deploy, atualizando o ALLOWED_HOSTS para ['*']

--- setup/settings.py
+++ setup/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = "django-insecure-&j08p!_m!+v2eqmoiku((d4xg(7%762ai-#)kqzqgf5!zf19e0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
